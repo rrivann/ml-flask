1f241072f322eda4b4e5348d95dd9a06a4645eb2
rrivann <rivanalbaniray88@gmail.com>
2023-08-18 09:56:32 +0700

    aq

---
+++ server.py
@@ -0,0 +1,34 @@
+import numpy as np
+from flask import Flask, request, jsonify
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+model = pickle.load(open('modeldiabet.pkl','rb'))
+
+@app.route('/api',methods=['POST']) 
+def predict():
+    data = request.get_json(force=True)
+
+    datajson = {
+    'age':[int(np.array(data['age']))],
+    'sex' :[int(np.array(data['sex']))],
+    'bmi':[int(np.array(data['bmi']))],
+    'highbp':[int(np.array(data['highbp']))],
+    'highchol' : [int(np.array(data['highchol']))],
+    'smoker' : [int(np.array(data['smoker']))],
+    'stroke' : [int(np.array(data['stroke']))],
+    'heartdisease' : [int(np.array(data['heartdisease']))],
+    'physactivity' : [int(np.array(data['physactivity']))],
+    'diffwalk' : [int(np.array(data['diffwalk']))]
+              }
+
+    df = pd.DataFrame(datajson)
+    prediction = model.predict(df)
+    output = prediction['diabetes'].values[0]
+    result = {}
+    result['hasil'] = str(output)
+    return jsonify(result) 
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
